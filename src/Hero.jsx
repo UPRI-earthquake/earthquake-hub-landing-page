c0715407a04cb6f8cfd1d1ce22cd09a405f9f868
samuel <chingcuangco.238863@munoz.sti.edu.ph>
2026-03-05 14:06:13 +0800

    added Publications in the nav bar

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,6 +4,7 @@ import banner from './assets/qqbanner.JPG'
 const navItems = [
   { label: 'Projects', targetId: 'projects' },
   { label: 'Community', targetId: 'community' },
+  { label: 'Publications', targetId: 'publications' },
   { label: 'Station View', targetId: 'station-view' },
   { label: 'Products', targetId: 'products' },
   { label: 'Contact Us', targetId: 'contact-us' },
